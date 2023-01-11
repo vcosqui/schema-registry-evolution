28e6fff6aae4b904e0ac47857ca85555d401d802
Victor Cosqui <vcosqui@confluent.io>
2023-01-11 12:40:02 +0100

    init version

---
+++ src/main/java/io/confluent/examples/clients/basicavro/ConsumerExample.java
@@ -0,0 +1,40 @@
+package io.confluent.examples.clients.basicavro;
+
+import com.google.common.collect.ImmutableList;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+public class ConsumerExample {
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void main(final String[] args) throws IOException {
+
+        final Properties props = new Properties();
+        try (InputStream inputStream = new FileInputStream("/Users/vcosqui/.confluent/java.config")) {
+            props.load(inputStream);
+        }
+
+        try (final KafkaConsumer<String, Payment> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(ImmutableList.of("transactions"));
+            while (true) {
+                final ConsumerRecords<String, Payment> records = consumer.poll(Duration.ofMillis(100));
+                for (final ConsumerRecord<String, Payment> record : records) {
+                    final String key = record.key();
+                    final Payment value = record.value();
+                    System.out.printf("key = %s, value = %s%n", key, value);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/io/confluent/examples/clients/basicavro/ProducerExample.java
@@ -0,0 +1,40 @@
+package io.confluent.examples.clients.basicavro;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Random;
+
+import static java.lang.Math.abs;
+import static java.lang.String.valueOf;
+import static java.text.MessageFormat.format;
+
+public class ProducerExample {
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void main(final String[] args) throws IOException {
+
+        final Properties props = new Properties();
+        try (InputStream inputStream = new FileInputStream("/Users/vcosqui/.confluent/java.config")) {
+            props.load(inputStream);
+        }
+
+        try (KafkaProducer<String, Payment> producer = new KafkaProducer<>(props)) {
+            for (long i = 0; i < 10; i++) {
+                final String orderId = format("id{0}{1}", valueOf(abs(new Random().nextInt())), i);
+                producer.send(
+                        new ProducerRecord<String, Payment>("transactions", orderId, new Payment(orderId, new Random().nextDouble())));
+                System.out.printf("payment sent with id %s\n", orderId);
+                Thread.sleep(1000L);
+            }
+            producer.flush();
+            System.out.printf("Successfully produced 10 messages to a topic called %s%n", "transactions");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
